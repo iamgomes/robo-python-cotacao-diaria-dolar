3c616ed2fd27454ddb15f60346d9723d9cc51f07
iamgomes <wgomes100@gmail.com>
2021-07-22 11:42:00 -0400

    Atualizando mensagens no console

--- robocotaçãodólar.py
+++ robocotaçãodólar.py
@@ -9,10 +9,17 @@ codSerie = '1'
 dataInicial = '01/01/1997'
 dataFinal = date.today().strftime('%d/%m/%Y')
 
+print('Período dos dados: {} a {}'.format(dataInicial, dataFinal))
+
+print('Acessando API do Banco Central...')
 url = 'http://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json&dataInicial={}&dataFinal={}'.format(codSerie,dataInicial,dataFinal)
 
+print('Retornando os dados...')
 df = pd.read_json(url)
 df.set_index('data', inplace=True)
 
+print('Salvando os dados em arquivo csv...')
 #Exporta a consulta em arquivo csv
 df.to_csv('PreçoDólar.csv', sep=';')
+
+print('Arquivo salvo com sucesso!')
